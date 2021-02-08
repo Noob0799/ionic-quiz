71078b03dac147ffc622df60c107d256488c32da
Noob0799 <uchihaitachi0799@gmail.com>
2021-02-08 18:52:48 +0530

    Play component card flipping functionality added

--- src/components/play/Play.tsx
+++ src/components/play/Play.tsx
@@ -1,54 +1,236 @@
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel } from '@ionic/react';
-import { documentText, pin, power, walk, warning, wifi, wine } from 'ionicons/icons';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonAlert,
+  withIonLifeCycle,
+} from "@ionic/react";
+import { documentText, power } from "ionicons/icons";
 import styles from "./Play.module.css";
-import React, { Fragment } from 'react';
-import data from './monuments.json';
-
-class Play extends React.Component {
-    handleRestart = () => {}
-    render() {
-        console.log(data.monuments);
-        return (
-            <Fragment>
-                <IonHeader>
-                    <IonToolbar color="warning">
-                        <IonTitle>Monument List <IonIcon icon={documentText}></IonIcon></IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent className={styles.content}>
-                    <div className={styles.container}>
-                    {
-                    data.monuments && data.monuments.length > 0 ?
-                    (
-                        data.monuments.map(obj => {
-                            return (
-                                <IonCard key={obj.id}>
-                                    <img src={obj.url} alt={obj.name}/>
-                                    <IonCardHeader>
-                                    <IonCardSubtitle>Name</IonCardSubtitle>
-                                    <IonCardTitle>{obj.name}</IonCardTitle>
-                                    <IonCardSubtitle>Destination</IonCardSubtitle>
-                                    <IonCardTitle>{obj.location}</IonCardTitle>
-                                    </IonCardHeader>
-                                </IonCard>
-                            )
-                        })
-                    ) : 
-                    (
-                        <h3>Game cannot be played right now.</h3>
-                    )
-                    }
-                    </div>
-                    <div className={styles.playbutton}>
-                    <IonButton color="warning" size="large" href="/play" onClick={this.handleRestart}>
-                    Restart
-                    <IonIcon slot="end" icon={power}></IonIcon>
-                    </IonButton>
-                    </div>
-                </IonContent>
-            </Fragment>
-        );
-    }
+import React, { Fragment } from "react";
+import data from "./monuments.json";
+import images from "./images";
+import ReactCardFlip from "react-card-flip";
+import TinderCard from "react-tinder-card";
+interface MyState {
+  showIntroAlert: boolean;
+  showExitAlert: boolean;
+  isFlipped: Array<boolean>;
+  monuments: Array<{ id: number; name: string; location: string }>;
+  displayData: Array<{ id: number; name: string; location: string }>;
+  score: number;
 }
 
-export default Play;
+class Play extends React.Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showIntroAlert: false,
+      showExitAlert: false,
+      isFlipped: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+      ],
+      monuments: [],
+      displayData: [],
+      score: 0
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      showIntroAlert: true,
+      monuments: [...data.monuments],
+      displayData: [...data.monuments]
+    });
+  }
+
+  handleRestart = () => {
+    this.setState({
+        displayData: [...this.state.monuments],
+        score: 0,
+        showIntroAlert: false,
+        showExitAlert: false,
+    });
+  };
+
+  setShowIntroAlert = (val: boolean) => {
+    this.setState({
+      showIntroAlert: val,
+    });
+  };
+
+  setShowExitAlert = (val: boolean) => {
+    this.setState({
+      showExitAlert: val,
+      score: 0,
+      showIntroAlert: true,
+      displayData: [...this.state.monuments],
+      isFlipped: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+      ]
+    });
+  };
+
+  handleFlip = (id: number) => {
+    const flipArr = [...this.state.isFlipped];
+    flipArr[id] = !flipArr[id];
+    this.setState({
+      isFlipped: [...flipArr],
+    });
+  };
+
+  onSwipe = (direction: string,id: number) => {
+    console.log("You swiped: " + direction);
+    let displayData: Array<{ id: number; name: string; location: string }> = [];
+    if(direction === 'left' || direction === 'right') {
+        displayData = this.state.displayData.filter(obj => {
+            return obj.id !== id
+        });
+    }
+    if(direction === 'right') {
+        this.setState({
+            score: this.state.score + 1,
+            displayData: [...displayData]
+        });
+    } else if(direction === 'left') {
+        this.setState({
+            displayData: [...displayData]
+        });
+    }
+  };
+
+  onCardLeftScreen = (direction: string) => {
+    console.log("You swiped: " + direction);
+    if(this.state.displayData.length === 0) {
+        this.setState({
+            showExitAlert: true,
+            displayData: [...this.state.monuments],
+            showIntroAlert: false,
+        });
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <IonHeader>
+          <IonToolbar color="warning">
+            <IonTitle>
+              Monument List <IonIcon icon={documentText}></IonIcon>
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className={styles.content}>
+          <IonAlert
+            isOpen={this.state.showIntroAlert}
+            onDidDismiss={() => this.setShowIntroAlert(false)}
+            cssClass={styles.alert}
+            header={"Instructions"}
+            message={
+              "Guess the locations of the monuments and tap on the cards to see if you got them correct. Swipe left if you got them wrong and right if you were spot on!"
+            }
+            buttons={["OK"]}
+          />
+          <IonAlert
+            isOpen={this.state.showExitAlert}
+            onDidDismiss={() => this.setShowExitAlert(false)}
+            cssClass={styles.alert}
+            header={"Score"}
+            message={
+                `${this.state.score}/10`
+            }
+            buttons={["OK"]}
+            />
+          <div className={styles.container}>
+            {this.state.monuments && this.state.monuments.length > 0 ? (
+              this.state.displayData.map((obj) => {
+                return (
+                  <div key={obj.id}>
+                    <ReactCardFlip
+                      isFlipped={this.state.isFlipped[obj.id - 1]}
+                      flipDirection="horizontal"
+                    >
+                      <div
+                        key="front"
+                        hidden={this.state.isFlipped[obj.id - 1]}
+                      >
+                        <IonCard onClick={() => this.handleFlip(obj.id - 1)}>
+                          <img src={images["image" + obj.id]} alt={obj.name} />
+                        </IonCard>
+                      </div>
+                      <div
+                        key="back"
+                        hidden={!this.state.isFlipped[obj.id - 1]}
+                      >
+                        <TinderCard
+                          onSwipe={(direction) => this.onSwipe(direction,obj.id)}
+                          onCardLeftScreen={this.onCardLeftScreen}
+                          preventSwipe={["up", "down"]}
+                        >
+                            <div className={styles.details}>
+                                <IonCardHeader
+                                onClick={() => this.handleFlip(obj.id - 1)}
+                                >
+                                <IonCardSubtitle>Name</IonCardSubtitle>
+                                <IonCardTitle>{obj.name}</IonCardTitle>
+                                <br />
+                                <IonCardSubtitle>Destination</IonCardSubtitle>
+                                <IonCardTitle>{obj.location}</IonCardTitle>
+                                </IonCardHeader>
+                            </div>
+                        </TinderCard>
+                      </div>
+                    </ReactCardFlip>
+                  </div>
+                );
+              })
+            ) : (
+                <h3>Game cannot be played right now!</h3>
+            )}
+          </div>
+          <div className={styles.playbutton}>
+            <IonButton
+              color="warning"
+              size="large"
+              href="/play"
+              onClick={this.handleRestart}
+            >
+              Restart
+              <IonIcon slot="end" icon={power}></IonIcon>
+            </IonButton>
+          </div>
+        </IonContent>
+      </Fragment>
+    );
+  }
+}
+
+export default withIonLifeCycle(Play);
---
+++ src/components/play/images.js
@@ -0,0 +1,15 @@
+import image1 from './monument_images/Taj-Mahal.jpg';
+import image2 from './monument_images/Qutub-Minar.jfif';
+import image3 from './monument_images/Konarka_Temple.jpg';
+import image4 from './monument_images/Fatehput_Sikiri_Buland_Darwaza_gate_2010.jpg';
+import image5 from './monument_images/1200px-Kailasha_temple_at_ellora.jfif';
+import image6 from './monument_images/Red-Fort.jfif';
+import image7 from './monument_images/Great-Stupa-Sanchi-India.jpg';
+import image8 from './monument_images/Jantar_Mantar,_New_Delhi_(Misra_Yantra).jpg';
+import image9 from './monument_images/depositphotos_116925416-stock-photo-mysore-palace-in-mysore-india.jpg';
+import image10 from './monument_images/Golden-Temple.jfif';
+
+
+const images: any = {image1,image2,image3,image4,image5,image6,image7,image8,image9,image10};
+
+export default images;
